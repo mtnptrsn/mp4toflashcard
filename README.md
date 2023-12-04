a4f80fcf7174e77ce08276a8ec4f807b9a957d5f
Mårten Pettersson <mtnptrsn@gmail.com>
2023-12-04 13:58:57 +0100

    add txt & url as input

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Using this video: https://www.youtube.com/watch?v=Tn6-PIqc4UM (React in 100 Seco
 
 1. **Command Line Arguments:**
 
-   - `-i` or `--input`: Path to the input MP4 file.
+   - `-i` or `--input`: Path to mp4 / path to txt file / url to txt file
    - `-o` or `--output`: Directory path for the output CSV file containing flashcards.
    - `-d` or `--duration`: Duration of the MP4 file to process (in seconds). Default is 60 seconds.
    - `-st` or `--starttime`: Start time for processing the MP4 file (in seconds). Default is 0.
--- main.py
+++ main.py
@@ -1,23 +1,82 @@
 import argparse
+import shlex
+import os
 import ffmpeg
 import whisper
 import openai
-import os
 from halo import Halo
+import requests
+
+MP3_TMP_PATH = "/tmp/transcript.mp3"
+TXT_TMP_PATH = "/tmp/transcript.txt"
+PROMPT = open("prompt.txt", "r").read()
+
+
+def is_url(s):
+    return s.startswith("http://") or s.startswith("https://")
+
+
+def download_file(url, content_type):
+    response = requests.get(url)
+    response_content_type = response.headers.get("Content-Type")
+
+    if response_content_type is None:
+        raise ValueError("The response does not contain a Content-Type header.")
+
+    if response_content_type.split(";")[0].strip() != content_type:
+        raise ValueError(
+            f"Expected Content-Type '{content_type}', but got '{response_content_type}' instead."
+        )
+
+    return response.content
+
+
+def strip_quotes(arg_value):
+    if arg_value:
+        return shlex.split(arg_value)[0]
+    return arg_value
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Convert MP4 to Flashcards')
-    parser.add_argument('-i', '--input', required=True,
-                        help='Input MP4 file path')
-    parser.add_argument('-o', '--output', required=True,
-                        help='Output directory for flashcards CSV file')
-    parser.add_argument('-d', '--duration', required=False,
-                        help='Duration', default=60)
-    parser.add_argument('-st', '--starttime', required=False,
-                        help='Start time', default=0)
-    parser.add_argument('-l', '--language', required=False,
-                        help='Language', default="en")
+    parser = argparse.ArgumentParser(description="Convert MP4/txt to Flashcards")
+    parser.add_argument(
+        "-i",
+        "--input",
+        required=True,
+        type=strip_quotes,
+        help="Input MP4/txt file path/url to txt file",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        required=True,
+        type=strip_quotes,
+        help="Output directory for flashcards CSV file",
+    )
+    parser.add_argument(
+        "-d",
+        "--duration",
+        required=False,
+        type=strip_quotes,
+        help="Duration",
+        default=60,
+    )
+    parser.add_argument(
+        "-st",
+        "--starttime",
+        required=False,
+        type=strip_quotes,
+        help="Start time",
+        default=0,
+    )
+    parser.add_argument(
+        "-l",
+        "--language",
+        required=False,
+        type=strip_quotes,
+        help="Language",
+        default="en",
+    )
     return parser.parse_args()
 
 
@@ -29,21 +88,18 @@ def mp3_to_text(mp3_file, language="en"):
 
 def mp4_to_mp3(mp4_file, mp3_file, start_time, duration):
     ffmpeg.input(mp4_file, ss=start_time, t=duration).output(
-        mp3_file, format='mp3').run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
+        mp3_file, format="mp3"
+    ).run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
 
 
 def text_to_flashcards(text):
-    prompt = "I want you to act as a anki flashcard creator. You are creating flashcards for students that are taking a course in a specific subject. You will be given a transcript of a lecture. I want you to create flashcards that are relevant to the course. I want you to respond with only CSV content without codeblock or any other text. Do not include a header row.\n\nHere is the transcript:\n\n" + text
-
     response = openai.ChatCompletion.create(
         model="gpt-4-1106-preview",  # or the specific model you want to use
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+        messages=[{"role": "user", "content": f"{PROMPT}\n\n{text}"}],
     )
 
     try:
-        flashcards_csv = response['choices'][0]['message']['content']
+        flashcards_csv = response["choices"][0]["message"]["content"]
         return flashcards_csv
     except Exception as e:
         print("Error in generating flashcards:", e)
@@ -53,20 +109,38 @@ def text_to_flashcards(text):
 if not os.getenv("OPENAI_API_KEY"):
     raise Exception("OPENAI_API_KEY environment variable is not set")
 
+
+def get_transcript(input, starttime, duration, language):
+    if input.endswith(".mp4"):
+        with Halo(text="Converting mp4 to mp3", spinner="dots"):
+            mp4_to_mp3(input, MP3_TMP_PATH, starttime, duration)
+        with Halo(text="Converting mp3 to text", spinner="dots"):
+            return mp3_to_text(MP3_TMP_PATH, language)
+
+    if input.endswith(".txt"):
+        with open(input, "r") as file:
+            return file.read()
+
+    if is_url(input):
+        with Halo(text="Downloading file", spinner="dots"):
+            return download_file(input, "text/plain")
+
+    raise Exception("Unsupported file format. Please provide an MP4 or TXT file.")
+
+
 args = parse_arguments()
 
-MP3_TMP_PATH = "/tmp/speech.mp3"
+transcript = get_transcript(
+    input=args.input,
+    starttime=args.starttime,
+    duration=args.duration,
+    language=args.language,
+)
 
-with Halo(text='Converting mp4 to mp3', spinner='dots'):
-    mp4_to_mp3(args.input, MP3_TMP_PATH, args.starttime, args.duration)
+with Halo(text="Generating flashcards", spinner="dots"):
+    flashcards_csv = text_to_flashcards(transcript)
 
-with Halo(text='Converting mp3 to text', spinner='dots'):
-    speech_text = mp3_to_text(MP3_TMP_PATH, args.language)
-
-with Halo(text='Generating flashcards', spinner='dots'):
-    flashcards_csv = text_to_flashcards(speech_text)
-
-with open(args.output, 'w') as file:
+with open(args.output, "w") as file:
     file.write(flashcards_csv)
 
 print(f"Flashcards CSV file generated at {args.output}")
